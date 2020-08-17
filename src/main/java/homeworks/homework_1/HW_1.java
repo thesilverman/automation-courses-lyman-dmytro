8b4fd1f264ca54d6765ff75b155733aeb62b05c6
Dmytro <thesilverman2@gmail.com>
2020-08-17 08:43:20 +0300

    change file

--- src/main/java/homeworks/homework_1/HW_1.java
+++ src/main/java/homeworks/homework_1/HW_1.java
@@ -1,7 +1,12 @@
 package homeworks.homework_1;
 
 public class HW_1 {
+    int a = 5;
+    int b = 10;
+
     public static void main(String[] args) {
-        System.out.println(System.getenv("browser"));
+        int b = 10;
+        int a = 20;
+        System.out.println(a * b);
     }
 }
