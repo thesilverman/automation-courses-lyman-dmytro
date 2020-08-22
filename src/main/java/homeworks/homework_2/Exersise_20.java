f38e9153795144dc2bac11e410fd14f80cde0125
Dmytro <thesilverman2@gmail.com>
2020-08-22 16:12:30 +0300

    next exersise

---
+++ src/main/java/homeworks/homework_2/Exersise_20.java
@@ -0,0 +1,17 @@
+package homeworks.homework_2;
+
+public class Exersise_20 {
+
+    //из цельсия в фаренгейт
+
+    public static void main(String[] args) {
+
+        double celsius = 25;
+
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println(fahrenheit);
+
+    }
+
+}
