ae04aac9369ab692391c22cba9e5fa6e648e0212
Dmytro <thesilverman2@gmail.com>
2020-09-14 22:55:37 +0300

    homework_7 plus fix Exersise_1 from homework_6

--- src/main/java/homeworks/homework_6/Exersise_1.java
+++ src/main/java/homeworks/homework_6/Exersise_1.java
@@ -6,11 +6,21 @@ public class Exersise_1 {
 
     public static void main(String[] args) {
 
-        String s = "hello world!";
+        String s = "hello world my friend!";
         String v = "HELLO WORLD!";
 
         chageRegisterToUppercase(s);
         changeRegisterToLowercase(v);
+        changeFirstLetter(s);
+    }
+
+    private static void changeFirstLetter(String s) {
+        String [] words = s.split("\\s");
+        String snew = "";
+        for (int i = 0; i < words.length; i++) {
+            snew = snew + words[i].substring(0,1).toUpperCase() + words[i].substring(1) + " ";
+        }
+        System.out.println("Первые буквы каждого слова = " + snew.trim());
     }
 
     private static void changeRegisterToLowercase(String v) {
---
+++ src/main/java/homeworks/homework_7/Exersise_1_main.java
@@ -0,0 +1,14 @@
+package homeworks.homework_7;
+
+public class Exersise_1_main {
+
+    public static void main(String[] args) {
+
+        Exersise_1_alpha_numeric_alphaNumeric stringon = new Exersise_1_alpha_numeric_alphaNumeric();
+        System.out.println(stringon.stringGenerator("alpha", 10));
+        System.out.println(stringon.stringGenerator("alphaNumeric", 10));
+        System.out.println(stringon.stringGenerator("numeric", 10));
+
+    }
+
+}
