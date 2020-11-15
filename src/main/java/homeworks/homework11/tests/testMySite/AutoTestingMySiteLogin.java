5fd6923227558b8255e6c195e9d9712be4caa443
Dmytro <thesilverman2@gmail.com>
2020-11-15 21:39:25 +0200

    "exercise Waits and ExecuteScript is done"

--- src/main/java/homeworks/homework11/tests/testMySite/AutoTestingMySiteLogin.java
+++ src/main/java/homeworks/homework11/tests/testMySite/AutoTestingMySiteLogin.java
@@ -1,32 +1,37 @@
 package homeworks.homework11.tests.testMySite;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.Assert.assertEquals;
 
 public class AutoTestingMySiteLogin {
 
     @Test
-    public void PositiveTestLoginIn() throws InterruptedException {
+    public void PositiveTestLoginIn() {
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 5, 1);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         driver.get("http://testautomationcuorses.iblogger.org/wordpress/");
         driver.manage().window().maximize();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlContains("wordpress/?i=1"));
+        WebElement loginLink = driver.findElement(By.xpath("//*[@id=\"meta-2\"]/ul/li[1]/a"));
+        js.executeScript("arguments[0].scrollIntoView();", loginLink);
         driver.findElement(By.xpath("//*[@id=\"meta-2\"]/ul/li[1]/a")).click();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlContains("wp-login.php"));
         driver.findElement(By.xpath("//*[@id=\"user_login\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//*[@id=\"user_pass\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//*[@id=\"loginform\"]/div/div/button/span")).click();
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.urlContains("wp-login.php"));
         driver.findElement(By.xpath("//*[@id=\"rememberme\"]")).click();
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.urlContains("wp-login.php"));
         driver.findElement(By.xpath("//*[@id=\"wp-submit\"]")).click();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlContains("/wp-admin/"));
+        js.executeScript("window.scrollBy(0,1000)");
         String actualLoginMessage = driver.findElement(By.xpath("//*[@id=\"wp-admin-bar-my-account\"]")).getText();
         assertEquals("Howdy, admin", actualLoginMessage);
         driver.quit();
@@ -36,11 +41,15 @@ public class AutoTestingMySiteLogin {
     @Test
     public void NegativeTestPasswordErrorMessage() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 5, 1);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         driver.manage().window().maximize();
         driver.get("http://testautomationcuorses.iblogger.org/wordpress/");
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlContains("wordpress/?i=1"));
+        WebElement loginLink = driver.findElement(By.xpath("//*[@id=\"meta-2\"]/ul/li[1]/a"));
+        js.executeScript("arguments[0].scrollIntoView();", loginLink);
         driver.findElement(By.xpath("//*[@id=\"meta-2\"]/ul/li[1]/a")).click();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlContains("wp-login.php"));
         driver.findElement(By.xpath("//*[@id=\"user_login\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//*[@id=\"wp-submit\"]")).click();
         String actualTestMessageError = driver.findElement(By.id("login_error")).getText();
@@ -52,11 +61,15 @@ public class AutoTestingMySiteLogin {
     @Test
     public void NegativeTestLoginErrorMessage() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 5, 1);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         driver.manage().window().maximize();
         driver.get("http://testautomationcuorses.iblogger.org/wordpress/");
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlContains("wordpress/?i=1"));
+        WebElement loginLink = driver.findElement(By.xpath("//*[@id=\"meta-2\"]/ul/li[1]/a"));
+        js.executeScript("arguments[0].scrollIntoView();", loginLink);
         driver.findElement(By.xpath("//*[@id=\"meta-2\"]/ul/li[1]/a")).click();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlContains("wp-login.php"));
         driver.findElement(By.xpath("//*[@id=\"user_pass\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//*[@id=\"wp-submit\"]")).click();
         String actualTestMessageError = driver.findElement(By.id("login_error")).getText();
@@ -67,11 +80,15 @@ public class AutoTestingMySiteLogin {
     @Test
     public void NegativeTestLoginPasswordMessageError() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 5, 1);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         driver.manage().window().maximize();
         driver.get("http://testautomationcuorses.iblogger.org/wordpress/");
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlContains("wordpress/?i=1"));
+        WebElement loginLink = driver.findElement(By.xpath("//*[@id=\"meta-2\"]/ul/li[1]/a"));
+        js.executeScript("arguments[0].scrollIntoView();", loginLink);
         driver.findElement(By.xpath("//*[@id=\"meta-2\"]/ul/li[1]/a")).click();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlContains("wp-login.php"));
         driver.findElement(By.xpath("//*[@id=\"wp-submit\"]")).click();
         String actualMessageErrorLoginPassword = driver.findElement(By.id("login_error")).getText();
         assertEquals("Error: The username field is empty.\n" +
@@ -82,22 +99,26 @@ public class AutoTestingMySiteLogin {
     @Test
     public void PositiveTestLoginOut() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 5, 1);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        driver.manage().window().maximize();
         driver.get("http://testautomationcuorses.iblogger.org/wordpress/");
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlContains("wordpress/?i=1"));
+        WebElement loginLink = driver.findElement(By.xpath("//*[@id=\"meta-2\"]/ul/li[1]/a"));
+        js.executeScript("arguments[0].scrollIntoView();", loginLink);
         driver.findElement(By.xpath("//*[@id=\"meta-2\"]/ul/li[1]/a")).click();
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.urlContains("wp-login.php"));
         driver.findElement(By.xpath("//*[@id=\"user_login\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//*[@id=\"user_pass\"]")).sendKeys("admin");
         driver.findElement(By.xpath("//*[@id=\"wp-submit\"]")).click();
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.urlContains("wp-admin"));
         WebElement ele = driver.findElement(By.xpath("//*[@id=\"wp-admin-bar-my-account\"]"));
         Actions actions = new Actions(driver);
         actions.moveToElement(ele).perform();
-        Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"wp-admin-bar-logout\"]/a")).click();
-        Thread.sleep(3000);
+        wait.until(ExpectedConditions.urlContains("wp-login.php?loggedout=true&wp_lang=en_US"));
         driver.findElement(By.xpath("//*[@id=\"reload-button\"]")).click();
-        Thread.sleep(2000);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"wp-submit\"]")));
         String actualMessageLogOut = driver.findElement(By.xpath("//*[@id=\"login\"]/p[1]")).getText();
         assertEquals("You are now logged out.", actualMessageLogOut);
         driver.quit();
